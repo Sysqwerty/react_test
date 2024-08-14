09ed4e5ff0f3c49f60a0dfedadcbf91e6996e504
Oleksii Dmytriiev <sysqwerty1984@gmail.com>
2024-08-14 19:28:19 +0300

    add example useSearchParams hook

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,12 +1,26 @@
+import { useSearchParams } from 'react-router-dom';
 import ProductList from '@components/ProductList';
+import SearchBox from '@components/SearchBox';
 import { getProducts } from '@/fakeApi';
 
 const Products = () => {
   const products = getProducts();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const productName = searchParams.get('name') ?? '';
+
+  const visibleProducts = products.filter(product =>
+    product.name.toLowerCase().includes(productName.toLowerCase())
+  );
+
+  const updateQueryString = name => {
+    const nextParams = name !== '' ? { name } : {};
+    setSearchParams(nextParams);
+  };
 
   return (
     <main>
-      <ProductList products={products} />
+      <SearchBox value={productName} onChange={updateQueryString} />
+      <ProductList products={visibleProducts} />
     </main>
   );
 };
